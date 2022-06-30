8b480393e468ff238e0f67ed8bdb47ddc25b37dc
vedant <ursvedantyetekar@gmail.com>
2022-06-30 20:30:55 +0530

    connected node to db

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
---
+++ server/models/consumer.js.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const consumerSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+consumerSchema.methods.generateAuthToke = function () {
+  const token = jwt.sign({ id: this._id }, "test", {
+    expiresIn: "7d",
+  });
+
+  return token;
+};
+
+const Consumer = mongoose.model("consumer", consumerSchema);
+
+const validate = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string().required().label("First Name"),
+    lastName: Joi.string().required().label("Last Name"),
+    email: Joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password"),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = { Consumer, validate };
+
+// 00:08:00
