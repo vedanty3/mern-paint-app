dfbd3acdeab8ab23a30afec97b677cee59e853d9
vedant <ursvedantyetekar@gmail.com>
2022-07-15 14:47:03 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 const app = express();
 
-app.listen(5000, () =>
+app.get("/", (req, res) => {
+  res.send("Troubleshooting...");
+});
+
+app.listen(PORT, () =>
   console.log(`Server running at port http://localhost:${PORT}/`)
 );
 
